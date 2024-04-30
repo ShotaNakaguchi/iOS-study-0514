6bd69c75e58e83d9c2d9bd86b0f957f5e242ba9c
shota.nakaguchi <shota.nakaguchi@kinto-technologies.com>
2024-04-30 13:27:57 +0900

    Initial Commit

---
+++ iOS-study-0514/iOS_study_0514App.swift
@@ -0,0 +1,21 @@
+//
+//  iOS_study_0514App.swift
+//  iOS-study-0514
+//
+//  Created by 中口 翔太（Shota Nakaguchi） on 2024/04/30.
+//
+
+import SwiftUI
+
+@main
+struct iOS_study_0514App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }
+    }
+}
